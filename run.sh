e1ad5876df5d108eba011a0646e0e5161ecadbd8
Peter C. Norton <spacey-github.com@ssr.com>
2022-07-25 15:47:44 -0400

    Run in the background

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# docker run --rm -d testserver --name "testserver-api-gateway"
-docker run --rm -p 5000:5000  --name "testserver-api-gateway" testserver
+# docker run --rm -p 5000:5000  --name "testserver-api-gateway" testserver
+docker run --rm -d -p 5000:5000  --name "testserver-api-gateway" testserver
