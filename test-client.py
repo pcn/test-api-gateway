e1518536e13bb98e6226300b19d3d3fbfb18ea03
Peter C. Norton <spacey-github.com@ssr.com>
2022-07-25 18:51:57 -0400

    Test with AWS auth headers

--- test-client.py
+++ test-client.py
@@ -1,6 +1,8 @@
 import requests
 import sys
 
+# Use aws-requests-auth from https://github.com/davidmuller/aws-requests-auth
+# in a virtualenv in /tmp/v
 
 hook_data = {
     "build_id": 5,
@@ -14,5 +16,36 @@ def test_no_aws_auth():
     print(r.status_code)
     print (r.text)
 
+# See https://github.com/boto/botocore/issues/1784
+def test_with_aws_auth():
+    from aws_requests_auth.aws_auth import AWSRequestsAuth
+    import boto3
+    hook_url = sys.argv[1]
+    profile_name = sys.argv[2]
+    role_arn = sys.argv[3]
+    hook_api_host =  "https://eq8hydgoha.execute-api.us-east-2.amazonaws.com/test-root".split("/")[2]
+    sess = boto3.Session(profile_name=profile_name)
 
-test_no_aws_auth()
+    creds = sess.client('sts').assume_role(RoleArn=role_arn, RoleSessionName='test')['Credentials']
+
+    auth = AWSRequestsAuth(
+        aws_access_key=creds['AccessKeyId'],
+        aws_secret_access_key=creds['SecretAccessKey'],
+        aws_token=creds['SessionToken'],
+        aws_region='us-east-2',
+        aws_service="execute-api",
+        aws_host=hook_api_host
+    )
+    #auth = AWSRequestsAuth(aws_access_key=creds.access_key,aws_secret_access_key=creds.secret_key,aws_token=creds.token,aws_region='us-east-2',aws_service="execute-api",aws_host=hook_api_host)
+
+
+    r = requests.post(hook_url, json={}, auth=auth)
+    print(r.status_code)
+    print (r.text)
+
+
+
+if sys.argv[1][0:5] == 'https':  # In AWS
+    test_with_aws_auth()
+else:
+    test_no_aws_auth()
